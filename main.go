96c49b1e822e5530da424afa7015c092a9483796
seoyul <19742930+resourceSaga@users.noreply.github.com>
2021-08-02 15:03:06 +0900

    recursive call

--- main.go
+++ main.go
@@ -4,21 +4,15 @@ import (
 	"fmt"
 )
 
-func Divide(a, b int) (result int, success bool) { // function declaration
-	// function definition
-	if b == 0 {
-		result = 0
-		success = false
+func printNo(n int) {
+	if n == 0 {
 		return
 	}
-	result = a / b
-	success = true
-	return
+	fmt.Println(n)
+	printNo(n - 1)
+	fmt.Println("After", n)
 }
 
 func main() {
-	c, success := Divide(9, 3)
-	fmt.Println(c, success)
-	d, success := Divide(9, 0)
-	fmt.Println(d, success)
+	printNo(3)
 }
