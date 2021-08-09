8e2c38172214e4cf408d9398d68b4dfa505fa7d4
seoyul <19742930+resourceSaga@users.noreply.github.com>
2021-08-09 16:32:00 +0900

    switch init

--- main.go
+++ main.go
@@ -1,14 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+func getMyAge() int {
+	return 11
+}
 
 func main() {
-	day := "thursday"
-
-	switch day {
-	case "monday", "tuesday":
-		fmt.Println("월, 화")
-	case "wedneday", "thurday", "friday":
-		fmt.Println("수, 목, 금")
+	// age 변수 선언 및 초기화
+	switch age := getMyAge(); { // 비굣값이 true이기 때문에 생략
+	case age < 10:
+		fmt.Println("child")
+	case age < 20:
+		fmt.Println("teenager")
+	case age < 30:
+		fmt.Println("20s")
+	default:
+		fmt.Println("My age is", age)
 	}
 }
