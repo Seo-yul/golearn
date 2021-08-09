ea173c03616433ef61fd14d8a2933dc1f0523c28
seoyul <19742930+resourceSaga@users.noreply.github.com>
2021-08-09 22:24:38 +0900

    for statement : continue, break

--- main.go
+++ main.go
@@ -1,23 +1,27 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
 func main() {
-	a := 3
-	switch a {
-	case 1:
-		fmt.Println("a == 1")
-		break
-	case 2:
-		fmt.Println("a == 2")
-	case 3:
-		fmt.Println("a == 3")
-		fallthrough
-	case 4:
-		fmt.Println("a == 4")
-	default:
-		fmt.Println("a > 4")
+	stdin := bufio.NewReader(os.Stdin)
+	for { // 무한 루프
+		fmt.Println("input")
+		var number int
+		_, err := fmt.Scanln(&number) // 한 줄 입력
+		if err != nil {               // 숫자가 아니면
+			fmt.Println("input number")
+
+			stdin.ReadString('\n') // 키보드 버퍼 제거
+			continue               // for문 처음으로
+		}
+		fmt.Printf("input number is %d \n", number)
+		if number%2 == 0 { // 짝수 검사
+			break // for문 종료
+		}
 	}
+	fmt.Println("for statement is end")
 }
